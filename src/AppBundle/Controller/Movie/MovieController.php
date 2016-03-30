3c522a8e683e684d76cfe3e8fb25efe0970e8a28
Stikwood <lalbatm@gmail.com>
2016-03-30 15:14:16 +0200

    Push des premiers fichiers

---
+++ src/AppBundle/Controller/Movie/MovieController.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace AppBundle\Controller\Movie;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MovieController extends Controller
+{
+    /**
+     * @Route("/list")
+     *
+     */
+    public function listAction()
+    {
+        return new Response('List of the movies');
+    }
+
+
+
+
+    /**
+     *
+     * @Route("/show/{id}", requirements={"id" = "\d+"})
+     *
+     */
+    public function showAction($id, Request $request)
+    {
+        //dump($request);die;
+
+        $tag = $request->query->get('tag');
+
+        return new Response(
+            'Affiche moi le film avec l\'id: '.$id.'avec le tag '.$tag
+        );
+    }
+}
--- src/AppBundle/Controller/Paint/PaintController.php
+++ src/AppBundle/Controller/Paint/PaintController.php
@@ -1,14 +1,14 @@
 <?php
 
 
-namespace AppBundle\Controller\Article; // OU se trouve physiquement notre fichier
+namespace AppBundle\Controller\Paint; // OU se trouve physiquement notre fichier
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController extends Controller
+class PaintController extends Controller
 {
     /**
      * @Route("/list")
@@ -16,8 +16,9 @@ class ArticleController extends Controller
      */
     public function listAction()
     {
-        return new Response('List of my new articles');
+        return new Response('List of my new paints');
     }
+
 
     // Création d'une nouvelle page => article/show/id
 
@@ -33,21 +34,7 @@ class ArticleController extends Controller
         $tag = $request->query->get('tag');
 
         return new Response(
-            'Affiche moi l\'article avec l\'id: '.$id.'avec le tag '.$tag
+            'Affiche moi la peinture avec l\'id: '.$id.'avec le tag '.$tag
         );
-    }
-
-    /**
-     * @Route("/show/{articleName}")
-     *
-     * @param $articleName
-     *
-     * @return Response
-     */
-    public function showArticleNameAction($articleName)
-    {
-        return $this->render('AppBundle:Article:index.html.twig', [
-            'articleName' => $articleName,
-        ]);
     }
 }
