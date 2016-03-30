103d53ffdb7a9fd03830c0efef5dbe77de100977
Stikwood <lalbatm@gmail.com>
2016-03-30 19:42:23 +0200

    Affichage articles

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Article\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,33 @@ class HomeController extends Controller
      */
     public function indexAction()
     {
-        $antispam = $this->get('antispam');
-        dump($antispam->isSpam('kmdjngjrngmqerjngenrgvnmvjmevnsjk'));die;
+        $manager = $this->getDoctrine()->getManager();
+
+        /*$article = new Article();
+
+        $article
+            ->setTitle('Titre article')
+            ->setContent('Le contenu de mon premier article')
+            ->setAuthor('Moi')
+            ->setTag('osef')
+        ;
+
+        $manager->persist($article);
+        $manager->flush();*/
+
+        $articleRepository = $manager->getRepository('AppBundle:Article\Article');
+
+        $articles = $articleRepository->findAll();
+
+
+
+        return $this->render('AppBundle:Home:index.html.twig', [
+            'articles' => $articles,
+        ]);
+
+
+        //$antispam = $this->get('antispam');
+        //dump($antispam->isSpam('kmdjngjrngmqerjngenrgvnmvjmevnsjk'));die;
         //return new Response('HomePage');
         //return $this->render('AppBundle:Home:index.html.twig');
     }
